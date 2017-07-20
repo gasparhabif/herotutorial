98add297652603af345aee5deef5c05a5314bfee
Gaspar Habif <gasparhabif@gmail.com>
2017-07-20 18:20:06 -0300

    Init Hero Tutorial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
 import { Component } from '@angular/core';
+import { Hero } from './components/hero';
+import { HeroService } from './services/hero.services';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [HeroService]
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Tour of Heros';
+  selectedHero: Hero;
+  heroes : Hero[];
+
+  constructor(private heroService: HeroService) { }
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
+  getHeroes(): void {
+    this.heroService.getHeroes().then(heroes => this.heroes = heroes);
+  }
+
+  ngOnInit(): void {
+    this.getHeroes();
+  }
 }
+
+//heroes: Hero[];
